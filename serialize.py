930414c791070fb6ed5714ba2abdb1af95dcc786
Yuxin Wu <ppwwyyxxc@gmail.com>
2022-12-23 06:49:50 -0800

    serializer cleanup

--- serialize.py
+++ serialize.py
@@ -42,27 +42,12 @@ class NumpySerializedList():
         bytes = memoryview(self._lst[start_addr:end_addr])
         return pickle.loads(bytes)
 
-class TorchSerializedList():
+
+class TorchSerializedList(NumpySerializedList):
     def __init__(self, lst: list):
-        self._lst = lst
-
-        def _serialize(data):
-            buffer = pickle.dumps(data, protocol=-1)
-            return np.frombuffer(buffer, dtype=np.uint8)
-
-        logger.info(
-            "Serializing {} elements to byte tensors and concatenating them all ...".format(
-                len(self._lst)
-            )
-        )
-        self._lst = [_serialize(x) for x in self._lst]
-        self._addr = np.asarray([len(x) for x in self._lst], dtype=np.int64)
-        self._addr = torch.from_numpy(np.cumsum(self._addr))
-        self._lst = torch.from_numpy(np.concatenate(self._lst))
-        logger.info("Serialized dataset takes {:.2f} MiB".format(len(self._lst) / 1024**2))
-
-    def __len__(self):
-        return len(self._addr)
+        super().__init__(lst)
+        self._addr = torch.from_numpy(self._addr)
+        self._lst = torch.from_numpy(self._lst)
 
     def __getitem__(self, idx):
         start_addr = 0 if idx == 0 else self._addr[idx - 1].item()
@@ -75,26 +60,10 @@ class TorchSerializedList():
 # has another implementation that does not use tensors.
 class TorchShmSerializedList(TorchSerializedList):
     def __init__(self, lst: list):
-        self._lst = lst
-
-        def _serialize(data):
-            buffer = pickle.dumps(data, protocol=-1)
-            return np.frombuffer(buffer, dtype=np.uint8)
-
         if comm.get_local_rank() == 0:
-            logger.info(
-                "Serializing {} elements to byte tensors and concatenating them all ...".format(
-                    len(self._lst)
-                )
-            )
-            self._lst = [_serialize(x) for x in self._lst]
-            self._addr = np.asarray([len(x) for x in self._lst], dtype=np.int64)
-            self._addr = torch.from_numpy(np.cumsum(self._addr))
-            self._lst = torch.from_numpy(np.concatenate(self._lst))
-            logger.info("Serialized dataset takes {:.2f} MiB".format(len(self._lst) / 1024**2))
+            super().__init__(lst)
         if comm.get_local_size() == 1:
             return
-
         if comm.get_local_rank() == 0:
             # Move to shared memory, obtain a handle.
             serialized = bytes(mp.reduction.ForkingPickler.dumps(
